912a32bc17b6416cf6a266232e519f2c2402cf5a
Moses <Moseschizaram8@gmail.com>
2024-02-21 02:39:01 +0100

    ran my first console based application

---
+++ MyCSharpProject/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Variables
+
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte number = 2;
+            Console.WriteLine(number);   
+        }
+    }    
+}
